b1c064daaa742500ec3098e221149c3b50d01c97
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-03-23 21:15:48 -0400

    second commit

---
+++ phenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\lhkni\Dropbox\Python\django-admin\producthunt-project\phenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
