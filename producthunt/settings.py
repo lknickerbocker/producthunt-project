56b1c62b47656b6f8285e0b1fbf39a34d9b0da03
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-03-24 21:06:12 -0400

    lets see what hapepns

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'producthuntrealdb',
         'USER': 'postgres',
-        'PASSWORD': '496808abc',
+        'PASSWORD': 'FUCKYOU',
         'HOST': 'localhost',
         'PORT': '5432',       
         
