7f0de5e895165f8349c514665fd14e9ac840f74b
Lance Knickerbocker <lhknickerbocker@gmail.com>
2020-03-29 18:32:35 -0400

    added forms and upload functionality static files

--- products/models.py
+++ products/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=500)
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default =1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='image/')
+    body = models.CharField(max_length=1000)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self): ###object title in database
+        return self.title
+
+    def summary(self): ####createsanewbodylimited to 100
+        return self.body[:100]
+
+    def pub_date_pretty(self): #customizes our date/time
+        return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+        if request.method == 'POST':
+            if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+                product = Product()
+                product.title = request.POST['title']
+                product.body = request.POST['body']
+                if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                    product.url = request.POST['url'] #then leave it alone
+                else:
+                    product.url = request.POST['url'] + request.POST['url']
+                product.icon = request.FILES['icon']
+                product.image = request.FILES['icon']
+                product.pub_date = timezone.datetime.now()
+                product.hunter = request.user
+                product.save() #actually inserts this into the database
+                return redirect('home')
+            else:
+                return render(request, 'products/create.html', {'error':'You must complete the form'})
+        else:
+            return render(request, 'products/create.html')
